b3b111e7473e29262ababb7cddfe219ac4ee7e3c
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-05-24 21:54:29 +0400

    Add files via upload

---
+++ lab2t1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab2t1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next());
+            Console.WriteLine(rnd.Next(5));
+            Console.WriteLine(rnd.Next(-5, 5));
+            Console.WriteLine("Нажмите на любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab2t2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace lab2t2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует рандомайзер, для продолжения введите диапазон выбора");
+            Console.Write("Введите начальное число диапазона: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите конечное число диапазона: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(a, b));
+            Console.WriteLine("Нажмите на любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
