44700501ff02d589350bbeab5a92382187993ef9
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-05-25 19:38:11 +0400

    Add files via upload

---
+++ lab2t22/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab2t22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите четырёхзначное число: ");
+            int num = int.Parse(Console.ReadLine());
+            int dgt1 = num / 1000;
+            int dgt2 = (num / 100) % 10;
+            int dgt3 = (num / 10) % 10;
+            int dgt4 = num % 10;
+            int summ = dgt1 + dgt2 + dgt3 + dgt4;
+            int x = dgt1 * dgt2 * dgt3 * dgt4;
+            Console.WriteLine($"Сумма цифр: {summ}");
+            Console.WriteLine($"Произведение цифр: {x}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab2t23/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace lab2t23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = b = c = 2;
+            double z = a * b + c++ / b - a * b * --c;
+
+            // а) Результат должен быть 3.66
+            a = b = c = 2;
+            z = (a * b) + ((double)c++ / b) - (a * b * --c);
+            Console.WriteLine($"а) Результат: {Math.Round(z, 2)}");
+
+            // б) Результат должен быть -1
+            a = b = c = 2;
+            z = (a * b + c++) / (b - a * b * --c);
+            Console.WriteLine($"б) Результат: {z}");
+
+            // в) Результат должен быть 2
+            a = b = c = 2;
+            z = (a * (b + c++) / b) - (a * b * --c);
+            Console.WriteLine($"в) Результат: {z}");
+
+            // г) Результат должен быть -2
+            a = b = c = 2;
+            z = a * (b + (c++ / (b - a * b * --c)));
+            Console.WriteLine($"г) Результат: {z}");
+
+            // д) Результат должен быть -4
+            a = b = c = 2;
+            z = (a * b + c++) / (b - a) * b * --c; // Я хотел сделать исключение, но решил что не нужно, ответ то правильный
+            Console.WriteLine($"д) Результат: {z}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
